d6fdfbe460eabecbca7b8e9f9782a91d60c18cb8
sdanil-ops <dsmirnov@psystems.biz>
2021-03-04 11:14:09 +0300

    fix: refactor code
    refactor code in stepik-7.3.10.py
    
    Issue#1

--- stepik/stepik-7.3.10.py
+++ stepik/stepik-7.3.10.py
@@ -1,7 +1,9 @@
-# program calculates sum of divisors of n
-n = int(input())
+#  Copyright (c) 2021. by Danil Smirnov
+#  zabanen.nu@ya.ru
+#  program calculates sum of divisors of n
+number = int(input())
 total = 0
-for i in range(1, n + 1):
-    if n % i == 0:
-        total += i
+for divisor in range(1, number + 1):
+    if number % divisor == 0:
+        total += divisor
 print(total)
