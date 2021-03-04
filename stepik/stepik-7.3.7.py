cdad45a50e63a63a2a02d909199f1057d033629a
sdanil-ops <dsmirnov@psystems.biz>
2021-03-04 11:12:34 +0300

    fix: refactor code
    refactor code in stepik-7.3.7.py
    
    Issue#1

--- stepik/stepik-7.3.7.py
+++ stepik/stepik-7.3.7.py
@@ -1,10 +1,12 @@
-# Write a program that calculates the sum of those numbers from 1 to n (inclusive)
-# whose square ends in 2, 5 or 8
+#  Copyright (c) 2021. by Danil Smirnov
+#  zabanen.nu@ya.ru
+#  Write a program that calculates the sum of those numbers from 1 to n (inclusive)
+#  whose square ends in 2, 5 or 8
 
-n = int(input())
+number = int(input())
 counter = 0
-for i in range(1, n + 1):
-    if i % 10 == 5: # scuare of any number cannot ends with 2 or 8
+for i in range(1, number + 1):
+    if i % 10 == 5:  # scuare of any number cannot ends with 2 or 8
         counter += i
 
 print(counter)
