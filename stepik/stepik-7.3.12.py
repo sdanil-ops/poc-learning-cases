f9bd8caf93fb964066f715cf6d459b00b2a32884
sdanil-ops <dsmirnov@psystems.biz>
2021-03-04 11:15:07 +0300

    fix: refactor code
    refactor code in stepik-7.3.12.py
    
    Issue#1

--- stepik/stepik-7.3.12.py
+++ stepik/stepik-7.3.12.py
@@ -1,16 +1,19 @@
-#
-n = int(input())
+#  Copyright (c) 2021. by Danil Smirnov
+#  zabanen.nu@ya.ru
+#  a program that prints the largest and
+#  second largest number of a sequence of numbers entered
+iterations_quantity = int(input())
 first_num = 0
 second_num = 0
 
-for i in range(n):
-    x = int(input())
-    if x > first_num:
+for i in range(iterations_quantity):
+    number = int(input())
+    if number > first_num:
         second_num = first_num
-        first_num = x
+        first_num = number
     else:
-        if x < first_num  and  x > second_num:
-            second_num = x
+        if first_num > number > second_num:
+            second_num = number
 
 print(first_num)
 print(second_num)
