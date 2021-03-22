52390fb54740bfdff489bea29e6e0a2ce233b73d
Danil Smirnov <77897780+sdanil-ops@users.noreply.github.com>
2021-03-22 18:44:08 +0300

    feat: add task 6.6

---
+++ pilschikov/PIL-6.6.py
@@ -0,0 +1,9 @@
+#  Copyright (c) 2021. by Danil Smirnov
+#  zabanen.nu@ya.ru
+#  напечатать текст образованный литералами с порядковыми
+#  номерами 65, 71 и 69
+
+chrds = [65, 71, 69]
+
+for i in range(len(chrds)):
+    print(chr(chrds[i]), end='')
