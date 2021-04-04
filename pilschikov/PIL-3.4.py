cd4a4662bc8c3b7ded775783826901f27e33df9c
Danil Smirnov <77897780+sdanil-ops@users.noreply.github.com>
2021-04-04 14:37:24 +0300

    refactor: refactor 3.4 task code

--- pilschikov/PIL-3.4.py
+++ pilschikov/PIL-3.4.py
@@ -1,9 +1,17 @@
-# task 3.4 page 14
+#  -----------------------------------------------------------
+#
+#  write a program that prints true or false depending
+#  on whether the given three integers have the same parity
+#
+#  Copyright (c) 2021. Danil Smirnov
+#  Released under GNU Public License (GPL)
+#  email zabanen.nu@ya.ru
+#  -----------------------------------------------------------
 even = 0
 add = 0
 
-for i in range(3):
-    n = int(input())
+for i in range(1, 4):
+    n = int(input(f'Input №{i} digit:\n'))
     if n % 2 == 0:
         even += n
     else:
