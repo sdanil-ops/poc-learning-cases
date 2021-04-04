55b40933fa68b6ab1a64838b58b71546629e1eac
sdanil-ops <dsmirnov@psystems.biz>
2021-04-04 15:40:11 +0300

    feat: add 3.13 task solution

---
+++ pilschikov/PIL.3.13.py
@@ -0,0 +1,29 @@
+#   -----------------------------------------------------------
+#
+#   Вычислить плошадь и периметр правильного 17-угольника,
+#   вписанного в  окружность заданного радиуса
+#
+#   Copyright (c) 2021. Danil Smirnov
+#   Released under GNU Public License (GPL)
+#   email zabanen.nu@ya.ru
+#   -----------------------------------------------------------
+import math
+# S = n2R2*sin2pi*n
+
+def get_poligon_area(radius, edges=17):
+    return ((edges/2) * radius ** 2) * math.sin((2 * math.pi) / edges)
+
+def get_poligon_perimetr(radius, edges=17):
+    angle = 360 / edges
+    return math.sqrt((radius ** 2) * 2 -(2 * (radius * radius)) * math.cos(angle))
+
+
+
+radius = int(input('Введите радиус окружности:\n'))
+
+print('Площадь правильного 17 угольника вписанного в окружность:')
+print(get_poligon_area(radius))
+print('Периметр правильного 17 угольника вписанного в окружность:')
+print(get_poligon_perimetr(radius))
+
+
