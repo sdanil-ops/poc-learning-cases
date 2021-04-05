bc303adf3a70068081ac5ca56052f7c581446bee
sdanil-ops <dsmirnov@psystems.biz>
2021-04-05 15:19:48 +0300

    refactor: refactor 6.6 task code

--- pilschikov/PIL-6.6.py
+++ pilschikov/PIL-6.6.py
@@ -3,7 +3,7 @@
 #  напечатать текст образованный литералами с порядковыми
 #  номерами 65, 71 и 69
 
-chrds = [65, 71, 69]
+chrs = [65, 71, 69]
 
-for i in range(len(chrds)):
-    print(chr(chrds[i]), end='')
+for i in range(len(chrs)):
+    print(chr(chrs[i]), end='')
